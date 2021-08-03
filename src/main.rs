fce7c8a21457767e9291fa9faec6e5fbd100abc7
martin.julien <martin.julien@cn.medecin.fr>
2021-08-03 14:35:54 +0200

    Refactor app to use db pooling

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,46 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
 
 use diesel::associations::HasTable;
 use diesel::prelude::*;
 use diesel::RunQueryDsl;
 use rocket::{get, routes};
-use rocket_contrib::json::Json;
 use rocket::response::status::NotFound;
+use rocket_contrib::databases::diesel;
+use rocket_contrib::json::Json;
 
-use hello_rocket::establish_connection;
 use hello_rocket::models::Person;
 use hello_rocket::schema::person::dsl::person;
 
 #[get("/<id>", format = "json")]
-fn find_one(id: i32) -> Result<Json<Person>, NotFound<String>> {
-    let connection = establish_connection();
-
-    let one: QueryResult<Person> = person.find(id).first(&connection);
+fn find_one(conn: PgDbConn, id: i32) -> Result<Json<Person>, NotFound<String>> {
+    let one: QueryResult<Person> = person.find(id).first(&*conn);
 
     println!("{:?}", one);
 
     match one {
         Ok(one) => Ok(Json(one)),
-        Err(one)=> Err(NotFound(one.to_string()))
+        Err(one) => Err(NotFound(one.to_string()))
     }
 }
 
 #[get("/", format = "json")]
-fn find_all() -> Json<Vec<Person>> {
-    let connection = establish_connection();
-
-    let all: QueryResult<Vec<Person>> = person::table().get_results(&connection);
+fn find_all(conn: PgDbConn) -> Json<Vec<Person>> {
+    let all: QueryResult<Vec<Person>> = person::table().get_results(&*conn);
 
     println!("{:?}", all);
 
     Json(all.ok().unwrap())
 }
 
+#[database("pg_db")]
+struct PgDbConn(diesel::PgConnection);
+
 fn main() {
-    rocket::ignite().mount("/", routes![find_one, find_all]).launch();
+    rocket::ignite()
+        .attach(PgDbConn::fairing())
+        .mount("/", routes![find_one, find_all])
+        .launch();
 }
